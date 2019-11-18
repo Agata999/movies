4eeddcc0058e42b8155cd2df09f78e804830f5f4
Agata Orzechowska <agata9orzechowska@gmail.com>
2019-11-18 18:24:22 +0100

    Add movie view

--- api/views.py
+++ api/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .serializers import UserSerializer
+from .serializers import UserSerializer, MovieSerializer
+from .models import Movie
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
