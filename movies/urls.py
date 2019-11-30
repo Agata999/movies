9e9ec15237195f4157c4110dce5f141a0e496141
Agata Orzechowska <agata9orzechowska@gmail.com>
2019-11-30 19:01:36 +0100

    Pagination and reformat with Black

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,6 @@ from api import urls
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(urls)),
+    path("admin/", admin.site.urls),
+    path("api/", include(urls)),
 ]
