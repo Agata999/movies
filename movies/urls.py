d4f3fd1cdff0e17629cfb9ed5a57fc846dfe90b1
Agata Orzechowska <agata9orzechowska@gmail.com>
2019-12-04 21:13:18 +0100

    Create tokens

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 from api import urls
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(urls)),
+    path("api-token-auth/", views.obtain_auth_token),
 ]
